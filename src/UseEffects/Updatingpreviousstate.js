86c2fa0d0edb187b5509170c7a4e0b8dfb744d1b
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-05-11 20:15:58 +0530

    add initial commit

--- src/UseEffects/Updatingpreviousstate.js
+++ src/UseEffects/Updatingpreviousstate.js
@@ -4,9 +4,14 @@ const Updatingpreviousstate = () => {
 
     const [count,setCount] = useState(0);
 
+   function handleClick()
+    {
+setCount(count+1);
+    }
+
 useEffect(()=>
 {
-    // console.log(count);
+    console.log(count);
     // const a=setInterval(()=>{
     //     setCount(b=>b+1);
     // },2000);
@@ -15,6 +20,7 @@ useEffect(()=>
 
   return (
     <>
+    <button onClick={handleClick}>ADD</button>
     <h1>{count}</h1>   </>
   )
 }
